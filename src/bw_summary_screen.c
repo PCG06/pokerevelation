bd6b1b1f39838e23463680203bfe5c5f169c87bd
PCG06 <pcggaming06@gmail.com>
2025-03-23 02:06:21 +0530

    Merge branch 'bw_summary_screen_expansion' of https://github.com/ravepossum/pokeemerald-expansion into upcoming

--- src/bw_summary_screen.c
+++ src/bw_summary_screen.c
@@ -351,7 +351,6 @@ static void HandleMonShinyIcon(bool8);
 static void HandleStatusSprite(struct Pokemon *);
 static u8 AddWindowFromTemplateList(const struct WindowTemplate*, u8);
 static void ClearCancelText(void);
-static void FormatTextByWidth(u8*, s32, u8, const u8*, s16);
 static void Task_ShowEffectTilemap(u8);
 static void Task_HideEffectTilemap(u8);
 static void HideInactivePageDots(void);
@@ -5393,7 +5392,7 @@ static void KeepMoveSelectorVisible(u8 firstSpriteId)
 }
 
 // Shoutout to Vexx for this :)
-static void FormatTextByWidth(u8 *result, s32 maxWidth, u8 fontId, const u8 *str, s16 letterSpacing)
+void FormatTextByWidth(u8 *result, s32 maxWidth, u8 fontId, const u8 *str, s16 letterSpacing)
 {
     u8 *end, *ptr, *curLine, *lastSpace;
 
