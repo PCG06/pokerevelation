574f694a4ec0d8355e4f9426c31711748afd5203
PCG06 <pcggaming06@gmail.com>
2025-05-26 00:44:14 +0530

    Elite Redux Select Battle Menu ported by AGSMGMaster64
    There is still a lot of stuff to fix and implement:
    Fix viewing enemy pokemon in summary.
    STAB and dynamic move types.
    BP modifiers.
    etc...

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -33,6 +33,7 @@
 #include "util.h"
 #include "window.h"
 #include "line_break.h"
+#include "ui_battle_menu.h"
 #include "constants/battle_anim.h"
 #include "constants/battle_move_effects.h"
 #include "constants/battle_partner.h"
@@ -2415,7 +2416,8 @@ static void Controller_WaitForDebug(u32 battler)
 static void PlayerHandleBattleDebug(u32 battler)
 {
     BeginNormalPaletteFade(-1, 0, 0, 0x10, 0);
-    SetMainCallback2(CB2_BattleDebugMenu);
+    FreeAllWindowBuffers();
+    UI_Battle_Menu_Init(ReshowBattleScreenAfterMenu);
     gBattlerControllerFuncs[battler] = Controller_WaitForDebug;
 }
 
